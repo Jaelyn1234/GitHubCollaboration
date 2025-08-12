181fcd24e16839ad29afc15f71d02e9021bc2b82
Jaelyn <jendiep@yahoo.com>
2025-08-12 13:51:33 -0400

    added hamstring and shin stretches to the leg view.

--- GitHubCollaboration/ContentView.swift
+++ GitHubCollaboration/ContentView.swift
@@ -11,68 +11,69 @@ struct ContentView: View {
     var body: some View {
         NavigationStack{
             
-    ZStack {
+            ZStack {
                 
-        Color(red: 0.8705882352941177, green: 0.8235294117647058, blue: 0.6666666666666666)
-            .ignoresSafeArea()
-        
-        VStack { //start of VStack
+                Color(red: 0.8705882352941177, green: 0.8235294117647058, blue: 0.6666666666666666)
+                    .ignoresSafeArea()
+                
+                VStack { //start of VStack
                     
-            HStack{
-                Text("Unknot")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .foregroundColor(Color(red: 0.4666666666666667, green: 0.43137254901960786, blue: 0.2784313725490196))
-            Spacer()
+                    HStack{
+                        Text("Unknot")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color(red: 0.4666666666666667, green: 0.43137254901960786, blue: 0.2784313725490196))
+                        Spacer()
                     } //end of HStack
-            Spacer()
+                    Spacer()
                     
                     
-            NavigationLink(destination: Arms()) {
-                Text("Arms")
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .foregroundColor(Color(red: 0.23, green: 0.296, blue: 0.384))
+                    NavigationLink(destination: Arms()) {
+                        Text("Arms")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color(red: 0.23, green: 0.296, blue: 0.384))
                         
                     } //end of arms navigation
-            Text("Elbows, shoulders, and wrists")
-            
-            Spacer()
+                    Text("Elbows, shoulders, and wrists")
                     
-            NavigationLink(destination: Legs()) {
-                Text("Legs")
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .foregroundColor(Color(red: 0.23, green: 0.296, blue: 0.384))
+                    Spacer()
+                    
+                    NavigationLink(destination: Legs()) {
+                        Text("Legs")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color(red: 0.23, green: 0.296, blue: 0.384))
                     } //end of legs navigation
                     
-            Text("Hamstrings, shins, calves, and quadriceps")
-            
-            Spacer()
+                    Text("Hamstrings, shins, calves, and quadriceps")
                     
-            NavigationLink(destination: Core()) {
-                Text("Core")
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .foregroundColor(Color(red: 0.23, green: 0.296, blue: 0.384))
+                    Spacer()
+                    
+                    NavigationLink(destination: Core()) {
+                        Text("Core")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color(red: 0.23, green: 0.296, blue: 0.384))
                     } //end of core navigation
                     
-            Text("Neck and back")
-            
-          //disclaimer
-            Text("This app does not constitute an attempt to practice medicine. The use of the app does not establish a professional-patient relationship. Individuals should consult a qualified health care provider for medical or other professional advice and answer to personal health questions. Instead, this app focuses on injury prevention.")
-                .font(.footnote)
-                .foregroundColor(Color.gray)
-                .multilineTextAlignment(.center)
-            
-
-        } //END of VStack
-
-        } // end of navigation stack
+                    Text("Neck and back")
+                    
+                    //disclaimer
+                    Text("This app does not constitute an attempt to practice medicine. The use of the app does not establish a professional-patient relationship. Individuals should consult a qualified health care provider for medical or other professional advice and answer to personal health questions. Instead, this app focuses on injury prevention.")
+                        .font(.footnote)
+                        .foregroundColor(Color.gray)
+                        .multilineTextAlignment(.center)
+                    
+                    
+                } //END of VStack
+                
+            } // end of navigation stack
             .padding()
-        
-    } //end of var body
-} //end of struct ContentView
+            
+        } //end of var body
+    } //end of struct ContentView
+}
 
 #Preview {
     ContentView()
