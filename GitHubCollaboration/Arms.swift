9cc60a1c1e632bd0bb618510b6567052ab8cf2b7
Kelly <kelvu14@gmail.com>
2025-08-13 11:13:03 -0400

    added rectangles to content view, edited disclaimer, added three stretches to arms

--- GitHubCollaboration/Arms.swift
+++ GitHubCollaboration/Arms.swift
@@ -14,6 +14,7 @@ struct Arms: View {
                 Text("  Arms")
                     .font(.largeTitle)
                     .fontWeight(.bold)
+                    .foregroundColor(Color(red: 0.23, green: 0.296, blue: 0.384))
                 Spacer()
             } //end of HStack
             
@@ -21,18 +22,66 @@ struct Arms: View {
             
             ZStack{
                 Text("Shoulder Stretch")
-                    .foregroundColor(Color.white)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color(red: 0.43137254901960786, green: 0.3568627450980392, blue: 0.29411764705882354))
             } //end of ZStack
             
             .padding()
             .background(Rectangle() .foregroundColor(Color(red: 0.8705882352941177, green: 0.8235294117647058, blue: 0.6666666666666666)))
             .cornerRadius(10)
             
-            Text("Description")
+            Image("Shoulder")
+                .resizable()
+                .padding(.all)
+                .frame(width: 300.0, height: 300.0)
+            
+            Text("Bring your left arm across your body and hold it with your right arm, either above or below the elbow. Hold for about 30 seconds. Switch arms and repeat.")
                 .font(.body)
-                .fontWeight(.light)
-                .foregroundColor(Color.gray)
-        
+                .fontWeight(.semibold)
+                .foregroundColor(Color(red: 0.43137254901960786, green: 0.3568627450980392, blue: 0.29411764705882354))
+                .padding(.horizontal)
+            
+            ZStack{
+                Text("Wall Stretch")
+                    .fontWeight(.bold)
+                    .foregroundColor(Color(red: 0.43137254901960786, green: 0.3568627450980392, blue: 0.29411764705882354))
+            } //end of ZStack
+            
+            .padding()
+            .background(Rectangle() .foregroundColor(Color(red: 0.8705882352941177, green: 0.8235294117647058, blue: 0.6666666666666666)))
+            .cornerRadius(10)
+            
+            Image("Wall")
+                .resizable()
+                .padding(.all)
+                .frame(width: 300.0, height: 300.0)
+            
+            Text("With palm open and fingers pointing away from body, put hand on the wall at shoulder height, keep elbow straight, and turn away from the wall to feel a stretch across the front of your arm/elbow. Hold for about 30 seconds. Switch arms and repeat.")
+                .font(.body)
+                .fontWeight(.semibold)
+                .foregroundColor(Color(red: 0.43137254901960786, green: 0.3568627450980392, blue: 0.29411764705882354))
+                .padding(.horizontal)
+            
+            ZStack{
+                Text("Chest Stretch")
+                    .fontWeight(.bold)
+                    .foregroundColor(Color(red: 0.43137254901960786, green: 0.3568627450980392, blue: 0.29411764705882354))
+            } //end of ZStack
+            
+            .padding()
+            .background(Rectangle() .foregroundColor(Color(red: 0.8705882352941177, green: 0.8235294117647058, blue: 0.6666666666666666)))
+            .cornerRadius(10)
+            
+            Image("Chest")
+                .resizable()
+                .padding(.all)
+                .frame(width: 300.0, height: 300.0)
+            
+            Text("Interlock your fingers behind you and push your arms up to stretch while standing. Interlock your fingers behind your head and move your elbows backward to do an above-the-head chest stretch. Hold for about 30 seconds.")
+                .font(.body)
+                .fontWeight(.semibold)
+                .foregroundColor(Color(red: 0.43137254901960786, green: 0.3568627450980392, blue: 0.29411764705882354))
+                .padding(.horizontal)
                 
                 Text("test color")
                     .foregroundColor(Color(red: 0.6588235294117647, green: 0.5490196078431373, blue: 0.49019607843137253))
